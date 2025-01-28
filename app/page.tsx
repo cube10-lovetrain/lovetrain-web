fa8f91a3854bb34b74baab29386c5d0af60b52bc
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-01-29 00:11:07 +0100

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from '@/components/Navbar';
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+	<div>
+	  <Navbar />
+	</div>
+  );
+};
+
+export default Page;
