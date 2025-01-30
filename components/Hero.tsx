618db8e0e02ae566617ae131b39d405b7ef6967a
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-01-30 11:06:50 +0100

    Updates hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,10 +3,10 @@ import React from "react";
 
 const Hero = () => {
 	return (
-		<section className="bg-gray-100 min-h-[90vh] flex items-center">
+		<section className="bg-gray-100 pt-2 md:pt-20 pb-28 flex items-center">
 			<div className="max-w-6xl mx-auto px-4 flex flex-col-reverse md:flex-row items-center justify-between gap-8">
 				{/* Left Column */}
-				<div className="flex-1 text-center md:text-left">
+				<div className="flex-1 text-center md:text-left pt-0 md:pt-32">
 					<div className="bg-transparent rounded-md md:bg-gradient-to-r from-[#ff9f1c] to-[#fdedd8] p-6">
 						<p className="text-[19px] sm:text-[20px] md:text-[30px] max-w-[300px] sm:max-w-[320px] md:max-w-[500px] font-bold text-black">
 							Join fundraising competitions Support your favorite cause Win
@@ -22,7 +22,7 @@ const Hero = () => {
 
 				{/* Right Column */}
 				<div className="flex-1 ">
-					<div className="w-full relative right-12 md:right-0 mt-0 lg:-mt-56">
+					<div className="w-full relative right-12 md:right-0">
 
 					<Image
 						src="/images/hero.svg"
