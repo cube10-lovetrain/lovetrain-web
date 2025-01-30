9de279925d6d7f09be8ab5ce5beeccd011bd644a
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-01-30 05:25:32 +0100

    Updates hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
 			<div className="max-w-6xl mx-auto px-4 flex flex-col-reverse md:flex-row items-center justify-between gap-8">
 				{/* Left Column */}
 				<div className="flex-1 text-center md:text-left">
-					<div className="bg-gradient-to-r from-[#ff9f1c] to-[#ffe2bb] p-6">
-						<p className="text-[20px] md:text-[30px] font-bold">
+					<div className="bg-transparent rounded-md md:bg-gradient-to-r from-[#ff9f1c] to-[#fdedd8] p-6">
+						<p className="text-[16px] sm:text-[18px] md:text-[30px] max-w-[300px] md:max-w-[450px] font-bold text-black">
 							Join fundraising competitions Support your favorite cause Win
 							amazing prizes
 						</p>
 					</div>
-					<div className="mt-6">
+					<div className="mt-3 md:mt-6">
 						<button className="px-6 py-3 bg-[#ff9f1c] text-white font-semibold rounded-full hover:bg-[#ffa527] transition duration-300">
 							Explore
 						</button>
@@ -21,14 +21,17 @@ const Hero = () => {
 				</div>
 
 				{/* Right Column */}
-				<div className="flex-1 mt-1 md:-mt-56">
+				<div className="flex-1 ">
+					<div className="w-full relative right-12 md:right-0 mt-0 lg:-mt-56">
+
 					<Image
 						src="/images/hero.svg"
 						alt="Hero Section Image"
-						className="mx-auto md:mx-0"
+						className="w-full"
 						width={900}
 						height={900}
 					/>
+					</div>
 				</div>
 			</div>
 		</section>
