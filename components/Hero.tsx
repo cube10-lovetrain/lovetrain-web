59199f10b1f24251a8e4f2874ce5dbb4c94dbe5f
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-01-30 07:08:47 +0100

    Updates hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -3,17 +3,17 @@ import React from "react";
 
 const Hero = () => {
 	return (
-		<section className="bg-gray-100 min-h-screen flex items-center">
+		<section className="bg-gray-100 min-h-[90vh] flex items-center">
 			<div className="max-w-6xl mx-auto px-4 flex flex-col-reverse md:flex-row items-center justify-between gap-8">
 				{/* Left Column */}
 				<div className="flex-1 text-center md:text-left">
 					<div className="bg-transparent rounded-md md:bg-gradient-to-r from-[#ff9f1c] to-[#fdedd8] p-6">
-						<p className="text-[16px] sm:text-[18px] md:text-[30px] max-w-[300px] md:max-w-[450px] font-bold text-black">
+						<p className="text-[19px] sm:text-[20px] md:text-[30px] max-w-[300px] sm:max-w-[320px] md:max-w-[500px] font-bold text-black">
 							Join fundraising competitions Support your favorite cause Win
 							amazing prizes
 						</p>
 					</div>
-					<div className="mt-3 md:mt-6">
+					<div className="mt-2 md:mt-6 ">
 						<button className="px-6 py-3 bg-[#ff9f1c] text-white font-semibold rounded-full hover:bg-[#ffa527] transition duration-300">
 							Explore
 						</button>
