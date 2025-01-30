794d375dd47697967e065a09caa98306d3748574
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-01-30 07:08:20 +0100

    Updates navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,82 +1,101 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
-import { Menu, X } from 'lucide-react';
-import Image from 'next/image';
+import { useState } from "react";
+import { Menu, X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <nav className="bg-white shadow-md">
-      <div className="max-w-6xl mx-auto">
-        <div className="flex justify-between items-center h-[80px] py-5">
-          {/* Logo */}
-          <div className="flex-shrink-0">
-            <Image src="/images/header-logo.png" alt='Charifun logo' width={200} height={200} className='w-[150px] h-[150px] md:w-[200px] md:h-[200px]' />
-          </div>
+	return (
+		<nav className="bg-white shadow-md">
+			<div className="max-w-6xl mx-auto">
+				<div className="flex justify-between items-center h-[80px] py-5">
+					{/* Logo */}
+					<div className="flex-shrink-0">
+						<Link href="/">
+							<Image
+								src="/images/header-logo.png"
+								alt="Charifun logo"
+								width={200}
+								height={200}
+								className="w-[150px] h-[150px] md:w-[200px] md:h-[200px]"
+							/>
+						</Link>
+					</div>
 
-          {/* Desktop Navigation */}
-          <div className="hidden md:flex items-center space-x-8">
-            <a href="#" className="text-[18px] text-gray-700 hover:text-[#ff9f1c] transition duration-300">Explore</a>
-            <a href="#" className="text-[18px] text-gray-700 hover:text-[#ff9f1c] transition duration-300">About</a>
-          </div>
+					{/* Desktop Navigation */}
+					<div className="hidden md:flex items-center space-x-8">
+						<a
+							href="#"
+							className="text-[18px] text-gray-700 hover:text-[#ff9f1c] transition duration-300"
+						>
+							Explore
+						</a>
+						<a
+							href="#"
+							className="text-[18px] text-gray-700 hover:text-[#ff9f1c] transition duration-300"
+						>
+							About
+						</a>
+					</div>
 
-          {/* Desktop Buttons */}
-          <div className="hidden md:flex items-center space-x-4">
-            <button className="px-4 py-2 text-gray-800 font-semibold border border-[#ff9f1c] rounded-full hover:bg-[#ff9f1c] transition duration-300">
-              Login
-            </button>
-            <button className="px-4 py-2 bg-[#ff9f1c] text-white font-semibold rounded-full hover:bg-[#ffa527] transition duration-300">
-              Sign Up
-            </button>
-          </div>
+					{/* Desktop Buttons */}
+					<div className="hidden md:flex items-center space-x-4">
+						<button className="px-4 py-2 text-gray-800 font-semibold border border-[#ff9f1c] rounded-full hover:bg-[#ff9f1c] transition duration-300">
+							Login
+						</button>
+						<button className="px-4 py-2 bg-[#ff9f1c] text-white font-semibold rounded-full hover:bg-[#ffa527] transition duration-300">
+							Sign Up
+						</button>
+					</div>
 
-          {/* Mobile menu button */}
-          <div className="md:hidden flex items-center">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-blue-600 focus:outline-none mr-4"
-            >
-              {isOpen ? (
-                <X className="h-6 w-6" />
-              ) : (
-                <Menu className="h-6 w-6" />
-              )}
-            </button>
-          </div>
-        </div>
+					{/* Mobile menu button */}
+					<div className="md:hidden flex items-center">
+						<button
+							onClick={() => setIsOpen(!isOpen)}
+							className="inline-flex items-center justify-center p-2 rounded-md text-gray-700 hover:text-blue-600 focus:outline-none mr-4"
+						>
+							{isOpen ? (
+								<X className="h-6 w-6" />
+							) : (
+								<Menu className="h-6 w-6" />
+							)}
+						</button>
+					</div>
+				</div>
 
-        {/* Mobile Navigation */}
-        {isOpen && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-3 space-y-1">
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-gray-700 hover:text-[#ff9f1c] transition duration-300"
-              >
-                Explore
-              </a>
-              <a
-                href="#"
-                className="block px-3 py-2 rounded-md text-gray-700 hover:text-[#ff9f1c] transition duration-300"
-              >
-                About
-              </a>
-              <div className="flex flex-col space-y-3 mt-4">
-                <button className="px-4 py-2 text-gray-800 border border-[#ff9f1c] rounded-full hover:bg-[#df9e44] w-full text-center">
-                  Login
-                </button>
-                <button className="px-4 py-2 bg-[#ff9f1c] text-white rounded-full hover:bg-[#ffa527] w-full text-center transition duration-300">
-                  Sign Up
-                </button>
-              </div>
-            </div>
-          </div>
-        )}
-      </div>
-    </nav>
-  );
+				{/* Mobile Navigation */}
+				{isOpen && (
+					<div className="md:hidden">
+						<div className="px-2 pt-2 pb-3 space-y-1">
+							<a
+								href="#"
+								className="block px-3 py-2 rounded-md text-gray-700 hover:text-[#ff9f1c] transition duration-300"
+							>
+								Explore
+							</a>
+							<a
+								href="#"
+								className="block px-3 py-2 rounded-md text-gray-700 hover:text-[#ff9f1c] transition duration-300"
+							>
+								About
+							</a>
+							<div className="flex flex-col space-y-3 mt-4">
+								<button className="px-4 py-2 text-gray-800 border border-[#ff9f1c] rounded-full hover:bg-[#df9e44] w-full text-center">
+									Login
+								</button>
+								<button className="px-4 py-2 bg-[#ff9f1c] text-white rounded-full hover:bg-[#ffa527] w-full text-center transition duration-300">
+									Sign Up
+								</button>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</nav>
+	);
 };
 
 export default Navbar;
