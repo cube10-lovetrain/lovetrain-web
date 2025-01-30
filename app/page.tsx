fe09c053513381f7212270a42fc42683923733cb
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-01-30 08:46:43 +0100

    Adds Mission component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
-import Hero from '@/components/Hero';
-import Navbar from '@/components/Navbar';
-import React from 'react';
+import Hero from "@/components/Hero";
+import HowItWorks from "@/components/HowItWorks";
+import Mission from "@/components/Mission";
+import WhyChari from "@/components/WhyChari";
+import React from "react";
 
 const Page: React.FC = () => {
-  return (
-	<div>
-	  <Navbar />
-    <Hero />
-	</div>
-  );
+	return (
+		<div>
+			<Hero />
+			<HowItWorks />
+			<WhyChari />
+      <Mission />
+		</div>
+	);
 };
 
 export default Page;
