a8ef69235440298946545af8684d099c41f195c0
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-02-02 00:33:52 +0100

    Adds how it works route

---
+++ app/how-it-works/page.tsx
@@ -0,0 +1,12 @@
+import HowItWorksPage from '@/components/how-it-works/HowItWorksPage'
+import React from 'react'
+
+const page = () => {
+  return (
+	<div>
+	  <HowItWorksPage />
+	</div>
+  )
+}
+
+export default page
