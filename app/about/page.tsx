d7294c93629a4544631582f06e5fd1c668a1167b
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-02-02 00:31:19 +0100

    Adds about route

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import AboutUs from '@/components/about/AboutUs'
+import Hero from '@/components/about/Hero'
+import WhyChariFun from '@/components/about/WhyChariFun'
+import React from 'react'
+
+const page = () => {
+  return (
+	<div>
+	  <Hero />
+	  <AboutUs />
+	  <WhyChariFun />
+	</div>
+  )
+}
+
+export default page
