7303459d5a8782c762e4220598990bc41df3df97
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-02-02 00:33:21 +0100

    Adds contact us route

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,14 @@
+import BecomePartner from '@/components/BecomePartner'
+import ContactUs from '@/components/ContactUs'
+import React from 'react'
+
+const page = () => {
+  return (
+	<div>
+	  <ContactUs />
+	  <BecomePartner />
+	</div>
+  )
+}
+
+export default page
