ed322055a564803320acdf8a3b26cb6a2d24b99e
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-02-02 00:35:49 +0100

    Adds new data

---
+++ data/privacy-policy.ts
@@ -0,0 +1,50 @@
+export const privacyData = [
+    {
+      title: "Privacy Policy",
+      content: "This privacy policy describes the relationship between Chari Fun Technology Inc. (\"Chari Fun\", \"we\", or \"us\"), the products and services operated by Chari Fun (collectively, such services, including any new features and applications, the \"Chari Fun Services\"), and the users of Chari Fun Services (\"you\" or \"your\")."
+    },
+    {
+      title: "A. Collection of Information",
+      content: `1. Chari Fun collects Personal Data from you when you voluntarily provide such information, such as when you contact Chari Fun with inquiries or register yourself or your company online for access to the Chari Fun Services.
+
+2. The type of information we may collect from you includes, for example, name, email address, address, phone number, and other information you choose to provide.
+
+3. If you provide Chari Fun with Personal Data, you acknowledge and agree that such Personal Data may be transferred from your current location to the offices and servers of Chari Fun and authorized third parties.`
+    },
+    {
+      title: "B. Use and Disclosure of Information",
+      content: `1. Your Chari Fun profile, the people and sources you follow, and the comments you post are all public.
+
+2. If you provide Personal Data for a certain reason, Chari Fun may use the Personal Data in connection with the reason for which it was provided.
+
+3. We use your information to keep Chari Fun running, better understand how you use our service, customize your experience, prevent abuse, and improve Chari Fun's products and services.`
+    },
+    {
+      title: "C. Storage and Information",
+      content: `1. We use cookies and HTML5 local storage to help us create your session, know when you're logged in, keep you logged in for your convenience, and help us remember certain information about how you interact with Chari Fun.
+
+2. Most browsers give you the option to turn off cookies and HTML5 local storage, or require your approval before using them to keep information.`
+    },
+    {
+      title: "D. Changes to the Privacy Policy",
+      content: "As Chari Fun continues to develop, we may decide to revise this privacy policy from time to time. The most current version will govern our use of your information, and older versions are available upon request."
+    },
+    {
+      title: "E. Consent and Your Rights",
+      content: `1. You provide your consent to the collection, use, and disclosure of personal information by us when you provide your information to us or use the Chari Fun Services.
+
+2. You can deny or withdraw your consent to our collection, use and disclosure of your personal information at any time upon reasonable notice, subject to any legal or contractual requirements.`
+    },
+    {
+      title: "F. Contact ChariFun",
+      content: "If you have questions, concerns, or feedback, about this privacy policy, or any other matters related to your privacy, please contact us at info@lovetrain.com"
+    },
+    {
+      title: "Additional Information For Residents of European Union",
+      content: "The European General Data Protection Regulation (GDPR) sets out obligations and rights with respect to the personal data of residents of the European Union. We are committed to ensuring that we comply with the requirements of the GDPR."
+    },
+    {
+      title: "Additional Information For Residents of California",
+      content: "The California Consumer Protection Act of 2018 (CCPA) sets out obligations and rights with respect to the Personal Data of California consumers. We are committed to ensuring that it complies with the requirements of the CCPA."
+    }
+  ];
---
+++ data/terms-and-condition.ts
@@ -0,0 +1,80 @@
+export const termsData = [
+	{
+		title: "Terms and Conditions",
+		content:
+			'Chari Fun Technology Inc. ("Chari Fun") offers you the opportunity to participate in fundraising campaigns for different causes and a chance to win prizes for your fundraising',
+	},
+	{
+		title: "1. How it works",
+		content: `a. Sponsors: Campaigns are hosted by ChariFun on its platform based in Vancouver, British Columbia, Canada. Campaigns for different causes are sponsored by the party indicated on the relevant Campaign Page (each, a "Sponsor"). Sponsors are typically third parties such as celebrities, non-profit organizations or charities, although ChariFun may be the sponsor in some cases.
+  
+  b. Campaign Page: Sponsors create pages with details about their Campaigns including the cause, fundraising goals, timelines, any prizes, etc. (the "Campaign Page").
+  
+  c. Supporters and Fundraisers: Each Campaign will run during the specific timeline set out on the relevant Campaign Page. During the Campaign, individuals, corporations, organizations, or other legal entities, may donate any amount to support the Campaign (anyone who donates to a Campaign is called a "Supporter").`,
+	},
+	{
+		title: "2. Eligibility",
+		content:
+			'a. Age. All individuals must be (a) at least 18 years old, or (b) the Age of Majority in their jurisdiction of residence, (the "Age of Majority") to participate in a Campaign.',
+	},
+	{
+		title: "3. Prizes",
+		content:
+			"a. If prizes are available, winner(s) will receive prizes as set out in the additional rules on the Campaign Page.",
+	},
+	{
+		title: "4. Charitable Donations and Platform Fee",
+		content:
+			"a. Each Campaign has a portion of its proceeds going to a cause specified in the relevant Campaign Page. In the event that the specified cause, if any, is not able to accept the donation, Sponsors may donate the charity of the revenue, if any, to an alternate cause of their choosing.",
+	},
+	{
+		title: "5. Conduct",
+		content:
+			"Failure to comply with these Terms and Conditions may result in disqualification. ChariFun and/or Sponsors reserve the right, in their sole and absolute discretion, to disqualify any individual acting in any manner deemed to be in violation of these Terms and Conditions.",
+	},
+	{
+		title: "6. Verification of Winner",
+		content:
+			'Winners may be required to execute eligibility, release and other documents ("Prize Claim Documents"), as may be required or permitted by law.',
+	},
+	{
+		title: "7. Privacy and Collection of Information",
+		content:
+			"By participating in a Campaign you consent to collection, use, and disclosure of your information by ChariFun, the Sponsor and each of their Related Entities in accordance with applicable laws and ChariFun's Privacy Policy available on the Website.",
+	},
+	{
+		title: "8. Disclaimer and Release of Liability",
+		content:
+			"ChariFun, SPONSORS AND THEIR RELATED ENTITIES ARE NOT RESPONSIBLE FOR ANY INCORRECT OR INACCURATE DONATION INFORMATION, HUMAN ERROR, TECHNICAL MALFUNCTION, FAILURES, OMISSION, INTERRUPTION, DELETION, OR DEFECT OF ANY TELEPHONE NETWORK.",
+	},
+	{
+		title: "9. Contact Information",
+		content:
+			"If you have any questions or comments about a Campaign as outlined above, or would like to request the name of the Winner(s), please contact ChariFun or the relevant Sponsor.",
+	},
+	{
+		title: "10. Release of Third Parties",
+		content:
+			"Campaigns are not in any manner sponsored, endorsed, or administered by, or associated with, YouTube, Facebook, Instagram or any other social media platform unless explicitly noted herein.",
+	},
+	{
+		title: "11. Terms of Use",
+		content:
+			"Campaigns are subject to the Terms of Use available on the Website and by participating in a Campaign you agree to the Terms of Use.",
+	},
+	{
+		title: "12. Force Majeure and Additional Sponsor Rights",
+		content:
+			"ChariFun and Sponsors reserve the right to cancel, terminate, modify, or suspend in whole or in part a Campaign at any time and in any way, without prior notice, for any reason in the sole discretion of ChariFun and Sponsors.",
+	},
+	{
+		title: "13. Governing Law and Venue",
+		content:
+			"All issues and questions concerning the construction, validity, interpretation, and enforceability of these Terms and Conditions or otherwise in connection with a Campaign shall be governed by, and construed in accordance with, the laws of the Province of British Columbia.",
+	},
+	{
+		title: "14. Severability",
+		content:
+			"If any provision of these Terms and Conditions shall be held invalid, illegal or unenforceable, such provision shall be enforced to the maximum extent permitted by law and ChariFun or Sponsor's fundamental intentions hereunder.",
+	},
+];
